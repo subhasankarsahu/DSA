37cedf789a4f1c4ec202fb15214ad8e7e1a7c107
Subha Sankar Sahu <subhasankarsahu5@gmail.com>
2026-06-24 14:06:29 +0530

    add reverse array

---
+++ Reverse_array.py
@@ -0,0 +1,15 @@
+def reverse_array(arr):
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        arr[left], arr[right] = arr[right], arr[left]
+
+        left += 1
+        right -= 1
+
+    return arr
+
+arr = [1,2,3,4,5,6,7,8,9,0]
+
+print(reverse_array(arr))
