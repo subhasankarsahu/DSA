451bad4a48286a1f17202493970d086678ea5c9d
Subha Sankar Sahu <subhasankarsahu5@gmail.com>
2026-07-01 12:37:53 +0530

    add Pattern4

---
+++ Patterns/Pattern4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def pattern4(self, n):
+        for i in range(1, n+1):
+            for j in range(1, i+1):
+                print(i, end=" ")
+            print()
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    n = 5
+
+    sol.pattern4(n)
+        
