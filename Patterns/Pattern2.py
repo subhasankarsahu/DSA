8df7be978bb360d4610e870c298bd1b559d5baf0
Subha Sankar Sahu <subhasankarsahu5@gmail.com>
2026-07-01 12:23:20 +0530

    Add Patterns

---
+++ Patterns/Pattern2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def pattern2(self, N):
+        for i in range(N):
+            print("* " * (i+1))
+
+if __name__ == "__main__":
+    sol = Solution()
+    N = 5
+    sol.pattern2(N)
