098933791e740159bbaaea0417bae86313e1585f
Subha Sankar Sahu <subhasankarsahu5@gmail.com>
2026-07-01 12:34:20 +0530

    add Pattern3

---
+++ Patterns/Pattern3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pattern3(self, n):
+        for i in range(1, n+1):
+            for j in range(1, i+1):
+                print(j, end=" ")
+
+            print()
+if __name__ == "__main__":
+    sol = Solution()
+
+    n = 5
+
+    sol.pattern3(n)
