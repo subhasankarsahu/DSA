d512657592c3317ab0979c716d6bd7f1d8c7880b
Subha Sankar Sahu <subhasankarsahu5@gmail.com>
2026-07-01 13:29:57 +0530

    add Pattern8

---
+++ Patterns/Pattern8.py
@@ -0,0 +1,27 @@
+
+class Solution:
+    def pattern5(self, n):
+        for i in range(n):
+            for j in range(i):
+                print(" ", end=" ")
+            
+            for j in range(2*n - (2*i + 1)):
+                print("*", end=" ")
+
+            print()
+        
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    n = 5
+
+    sol.pattern5(n)
+    
+'''
+* * * * * * * * * 
+  * * * * * * * 
+    * * * * * 
+      * * * 
+        * 
+'''
